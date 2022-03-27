22437e42c2daee19f77531cc536bc77e086ea606
ninaiavid <92196867+ninaiavid@users.noreply.github.com>
2022-03-27 19:16:25 +0300

    Delete JS-mid/Slot game - V2 directory

--- JS-mid/Slot game - V2/src/components/SlotGameWheel.js
+++
@@ -1,6 +0,0 @@
-const SlotGameWheel = (rootElement) => {
-    
-    let divWheel = createElement('div', {class: 'slot-game--wheel'}, rootElement)
-    
-    load('src/components/SlotGameWheel.css')
-}
